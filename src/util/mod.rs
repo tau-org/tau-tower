eee6d0a25245d78ee7497c9c2c89336e1ee9ceaf
Viktor Sandström <Viktor Sandstrm>
2025-12-09 20:09:12 +0100

    cleanup

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -21,10 +21,6 @@ impl Credentials {
 pub struct Headers {pub headers: Option<Bytes>}
 
 impl Headers {
-  // pub fn prepare_headers(&mut self, buf: &[Bytes]) {
-  //   self.headers = Some(Bytes::copy_from_slice(&[&buf[0][..], &buf[1][..]].concat()))
-  // }
-
   pub fn prepare_headers(&mut self, buf: &(&Bytes, &Bytes)) {
     self.headers = Some(Bytes::copy_from_slice(&[&buf.0[..], &buf.1[..]].concat()))
   }
@@ -54,13 +50,3 @@ pub fn validate_header(data: Bytes) -> Result<Option<Bytes>, ()> {
   }
   Err(())
 }
-
-// pub fn validate_bos_and_tags(data: &Bytes) -> core::result::Result<&Bytes, ()> {
-//   let n_segs = data[26] as usize;
-//   let offset = 27+n_segs;
-//   if data.len() < 27 + 8 { return Err(()) }
-//   if matches!(&data[offset..offset+8], b"OpusTags" | b"OpusHead") {
-//     return Ok(data);
-//   }
-//   Err(())
-// }
